68a5774923c199d47d0ba53d3931049f5ce8fceb
nico <ni.premont@gmail.com>
2026-01-15 16:43:44 +0100

    small fix mobile

--- src/i18n/translations/fr.ts
+++ src/i18n/translations/fr.ts
@@ -137,7 +137,7 @@ export const fr = {
   projects: {
     title: 'Projets\nselectionnés',
     otherProjects: 'Autres projets',
-    inProgress: '( on y travaille )',
+    inProgress: '(en cours...)',
   },
 
   // Footer
